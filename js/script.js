13c2e844e4bb623a0bf60fab38d72282371f352d
febrianhenska <febrianhenskaa@gmail.com>
2024-08-15 14:36:50 +0700

    done

--- js/script.js
+++ js/script.js
@@ -6,12 +6,14 @@ const form = document.querySelector("form");
 form.addEventListener("submit", onFormSubmit);
 form.addEventListener("reset", onFormReset);
 
+// function reset
 function onFormReset() {
   bmiText.textContent = 0;
   bmiText.className = "";
   descText.textContent = "N/A";
 }
 
+// function submit/bmi count
 function onFormSubmit(e) {
   e.preventDefault();
   const weight = parseFloat(form.weight.value);
@@ -22,14 +24,15 @@ function onFormSubmit(e) {
   }
 
   const heightInMeters = height / 100; // cm to meters
-  const bmi = weight / Math.pow(heightInMeters, 2);
-  const desc = interpretBMI(bmi);
+  const bmi = weight / Math.pow(heightInMeters, 2); // bmi = weight / height^2
+  const desc = interpretBMI(bmi); // healthy, overweight, obese
 
   bmiText.textContent = bmi.toFixed(2);
   bmiText.className = desc;
   descText.innerHTML = `You are <strong>${desc}</strong>`;
 }
 
+// function interpret/classification
 function interpretBMI(bmi) {
   if (bmi < 18.5) {
     return "underweight";
